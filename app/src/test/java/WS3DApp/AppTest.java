d3a96ea3828c5feaa0031f38688870da72fac0d9
GSSonoda <gustavoshoitisonoda@hotmail.com>
2025-04-02 14:58:39 -0300

    remove test

--- app/src/test/java/WS3DApp/AppTest.java
+++ app/src/test/java/WS3DApp/AppTest.java
@@ -6,9 +6,3 @@ package WS3DApp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
-}
